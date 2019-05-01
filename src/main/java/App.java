a5e95ac15c718a498a194f9c955aa12d5c76936c
iamkosgei <kosgeik2930@gmail.com>
2019-05-01 18:00:04 +0300

    added the root,forum and the timer routes.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,53 @@
+import org.apache.log4j.BasicConfigurator;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        BasicConfigurator.configure();
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
+
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/index.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
+
+        get("/forum", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/forum.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
+
+        get("/set-timer", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/set-timer.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
+
+
+    }
 }
