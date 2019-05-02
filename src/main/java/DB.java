2e192f354ad1ae454c7e21891ff69ef0206446c7
iamkosgei <kosgeik2930@gmail.com>
2019-05-02 13:10:07 +0300

    Created a connection to the database

---
+++ src/main/java/DB.java
@@ -0,0 +1,29 @@
+
+import org.sql2o.Sql2o;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class DB {
+    private static URI dbUri;
+    public static Sql2o sql2o;
+
+    static {
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://kosgei:12345678@localhost:5432/meditation_db");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+
+        } catch (URISyntaxException e) {
+
+        }
+    }
+}
